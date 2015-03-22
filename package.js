e78e0012aa2427803d6e62b609c037f23af221be
particle4dev <particle4dev@gmail.com>
2015-03-22 12:24:19 +0700

    release 0.19.0

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
     summary: "Cheerio for meteor",
-    version: "0.18.0-rc1",
+    version: "0.19.0",
     name: "particle4dev:cheerio",
     git: "https://github.com/particle4dev/meteor-cheerio.git"
 });
 
-Npm.depends({cheerio: "0.18.0"});
+Npm.depends({cheerio: "0.19.0"});
 
 Package.on_use(function (api) {
 	api.versionsFrom("1.0");
